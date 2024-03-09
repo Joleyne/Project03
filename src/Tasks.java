b3e44e3819829f609aa01efd0143859428ab7b3b
Joleyne Hernandez <livevil123456789@gmail.com>
2024-03-08 23:55:56 -0500

    Initial commit

---
+++ src/Tasks.java
@@ -0,0 +1,42 @@
+public class Tasks {
+    private String name;
+    private String description;
+    private int priority;
+
+    public Tasks(String name, String description, int priority) {
+        this.name = name;
+        this.description = description;
+        this.priority = priority;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    @Override
+    public String toString() {
+        return "Task: " + this.getName() +
+                "\nDescription: " + this.getDescription() +
+                "\nPriority: " + this.getPriority();
+    }
+}
